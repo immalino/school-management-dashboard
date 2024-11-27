e44854d1e4273dbcbada0d5fee0caca010d55d3f
immalino <fazadandyakmalin0@gmail.com>
2024-11-27 11:53:44 +0700

    create count cart componenent

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,4 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -12,7 +13,20 @@ const AdminPage = () => {
           <UserCard type="parent" />
           <UserCard type="staff" />
         </div>
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* BOTTOM CHART */}
+        <div className="w-full h-[500px]"></div>
       </div>
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3 flex flex-col gap-8"></div>
     </div>
   );
 };
